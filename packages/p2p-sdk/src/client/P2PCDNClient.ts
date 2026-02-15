8fe739b938f4948fe6667effa3fa811d7bd15717
Ishagi-Yoichi <nikunjkr1752003@gmail.com>
2026-02-15 22:03:09 +0530

    webRTC-manager 02

--- packages/p2p-sdk/src/client/P2PCDNClient.ts
+++ packages/p2p-sdk/src/client/P2PCDNClient.ts
@@ -1,11 +1,12 @@
 import { SwarmManager } from "../swarm/SwarmManager";
+import { WebRTCManager } from "../webrtc/WebRTCManager";
 
 const peerId = crypto.randomUUID();
 
 const swarm = new SwarmManager("ws://localhost:8080", peerId);
+const webrtc = new WebRTCManager(peerId, swarm);
 
 swarm.connect();
-
 swarm.join("video-abc");
 
 swarm.on("peerJoined", (peerId: any) => {
